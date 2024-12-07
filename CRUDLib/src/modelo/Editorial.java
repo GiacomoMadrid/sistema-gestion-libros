10a0687ad0f08633311e03d2ba2cb180f5cf6773
GiacomoMadrid <gsmr.18@gmail.com>
2024-12-07 18:09:56 -0500

    class Editorial

--- CRUDLib/src/modelo/Editorial.java
+++ CRUDLib/src/modelo/Editorial.java
@@ -5,5 +5,32 @@ package modelo;
  * @author Giacomo
  */
 public class Editorial {
+    int id;
+    String nombre;
+    
+    public Editorial(int id, String nombre){
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+    
+    
     
 }
