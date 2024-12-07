aeab7917aa75f892b05c6e6bf21586b5102d3416
GiacomoMadrid <gsmr.18@gmail.com>
2024-12-07 18:05:31 -0500

    class NoDataException

--- CRUDLib/src/conexion/NoDataException.java
+++ CRUDLib/src/conexion/NoDataException.java
@@ -1,13 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package conexion;
 
 /**
  *
  * @author Giacomo
  */
-public class NoDataException {
+public class NoDataException extends java.lang.Exception{
+    public NoDataException (){
+    
+    }
+    
+    public NoDataException (String mensaje){
+        super(mensaje);
+    }
     
 }
