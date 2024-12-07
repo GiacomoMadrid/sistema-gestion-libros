8ab3cad22ad523ca06c7560bd1a4b734c8c25ea1
GiacomoMadrid <gsmr.18@gmail.com>
2024-12-07 18:43:54 -0500

    class PaisDAO

---
+++ CRUDLib/src/conexion/PaisDAO.java
@@ -0,0 +1,76 @@
+package conexion;
+
+import java.util.Collection;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.CallableStatement;
+import java.util.ArrayList;
+import modelo.Pais;
+import oracle.jdbc.OracleTypes;
+/**
+ *
+ * @author Giacomo
+ */
+public class PaisDAO extends Conexion{
+    private static final String MOSTRAR_PAISES ="?=call mostrar_paises()" ;
+    
+    public Collection mostrar_paises() throws GlobalException, NoDataException {
+        try{
+            conectar();
+        
+        }catch(ClassNotFoundException ex){
+            throw new GlobalException("No se ha localizado el driver"); 
+            
+        }catch(SQLException exe){
+            throw new NoDataException("La base de datos no se encuentra disponible");
+        }
+        
+        ResultSet rs = null;
+        ArrayList lista = new ArrayList();
+        Pais paisObj = null;
+        CallableStatement pstmt =null; 
+        
+        try{
+            pstmt = conexion.prepareCall(MOSTRAR_PAISES);
+            pstmt.registerOutParameter(1, OracleTypes.CURSOR);
+            pstmt.execute();
+            
+            rs = (ResultSet)pstmt.getObject(1);
+            
+            while(rs.next()){
+               paisObj = new Pais(rs.getInt("id"), rs.getString("nombre"));
+               lista.add(paisObj);
+            }
+            
+        }catch(SQLException ex){
+            ex.printStackTrace();
+            throw new GlobalException("Sentencia inválida");
+        
+        }finally{
+            try{
+                if(rs == null){
+                    rs.close();
+                }
+                
+                if(pstmt == null){
+                    pstmt.close();
+                }
+                
+                desconectar();
+                
+            }catch(SQLException se){
+                throw new GlobalException("Estatutos no válidos");
+            
+            }
+        
+        }
+        
+        
+        if(lista == null || lista.size() == 0){
+            throw new NoDataException("No hay datos");
+        }
+        
+        return lista;
+    }
+    
+}
