abc0a4d38d95ab7001e3f142f7f40fc5b877627e
GiacomoMadrid <gsmr.18@gmail.com>
2024-12-07 18:04:45 -0500

    class GlobalException

--- CRUDLib/src/conexion/GlobalException.java
+++ CRUDLib/src/conexion/GlobalException.java
@@ -1,13 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package conexion;
 
 /**
  *
  * @author Giacomo
  */
-public class GlobalException {
+public class GlobalException extends java.lang.Exception{
+    public GlobalException (){
+    
+    }
+    
+    public GlobalException (String mensaje){
+        super(mensaje);
+    }
     
 }
