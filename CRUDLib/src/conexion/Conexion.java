6e1570b3f0895e1b0955f02f374629c80ba9a6d6
GiacomoMadrid <gsmr.18@gmail.com>
2024-12-07 18:02:05 -0500

    class Conexion

--- CRUDLib/src/conexion/Conexion.java
+++ CRUDLib/src/conexion/Conexion.java
@@ -1,13 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package conexion;
 
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 /**
  *
  * @author Giacomo
  */
 public class Conexion {
+    protected Connection conexion = null;
+    
+    public Conexion(){
+    }
+    
+    
+    protected void conectar()throws SQLException, ClassNotFoundException{
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+        conexion = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "system", "1234");
+    
+    }
+    
+    protected void desconectar()throws SQLException{
+        if(!conexion.isClosed()){
+            conexion.close();
+        }
+    }
     
 }
